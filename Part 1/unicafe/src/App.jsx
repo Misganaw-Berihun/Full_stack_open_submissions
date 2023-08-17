ca4654cc2b44f02a2b20f1e23af7bc16f64b54f9
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-08-17 22:16:08 +0300

    New br (#3)
    * Exercise 1.3 - 1.5 submission
    
    * Exercise 1.6
    
    * Excercise 1.7
    
    * Excercise 1.7 - 1.10

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
-const Display = (props) => {
+
+const StatisticLine = (props) => {
   console.log(props)
   return (
     <div>
-      <p>
-        {props.text} {props.count}
-      </p>
+      {props.text} {props.text === 'positive'? `${props.value} %` : props.value}
     </div>
   )
+}
+
+const Statisitcs = (props) => {
+  console.log(props)
+  if (props.total === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  } else{
+    return (
+      <div>
+        <StatisticLine text = 'good' value = {props.good}/>
+        <StatisticLine text = 'neutral' value = {props.neutral}/>
+        <StatisticLine text = 'bad' value = {props.bad}/>
+        <StatisticLine text = 'total' value = {props.total}/>
+        <StatisticLine text = 'average' value = {props.average}/>
+        <StatisticLine text = 'positive' value = {props.positive}/>
+      </div>
+    )
+  }
 }
 
 const Button = (props) => {
@@ -26,10 +47,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positiveFeedbackPercentage, setPositiveFeedbackPercentage] = useState(0)
 
-  const incrementGood = () => setGood(good + 1);
-  const incrementNeutral = () => setNeutral(neutral + 1);
-  const incrementBad = () => setBad(bad + 1);
+  const incrementGood = () => {
+    let updateGood = good + 1;
+    let total = updateGood + bad + neutral;
+    let avg = (updateGood - bad) / total;
+    let posFeedbackPercent = (updateGood * 100) / total; 
+
+    setGood(updateGood)
+    setTotal(total)
+    setAverage(avg);
+    setPositiveFeedbackPercentage(posFeedbackPercent);
+  }
+  const incrementNeutral = () => {
+    let updateNeutral = neutral + 1
+    let total = updateNeutral + bad + good;
+    let avg = (good - bad) / total;
+    let posFeedbackPercent = (good * 100) / total;
+
+    setNeutral(updateNeutral)
+    setTotal(total)
+    setAverage(avg);
+    setPositiveFeedbackPercentage(posFeedbackPercent);
+  }
+  const incrementBad = () => {
+    let updatedBad = bad + 1;
+    let total = updatedBad + good + neutral;
+    let avg = (good - updatedBad) / total;
+    let posFeedbackPercent = (good * 100) / total;
+
+    setBad(updatedBad);
+    setTotal(updatedBad + neutral + good);
+    setAverage(avg);
+    setPositiveFeedbackPercentage(posFeedbackPercent);
+  }
   
   return (
     <div>
@@ -38,9 +92,7 @@ const App = () => {
       <Button type = 'neutral' handleOnClick = {incrementNeutral}/ >
       <Button type = 'bad' handleOnClick = {incrementBad}/ >
       <h1>statistics</h1>
-      <Display text = 'good' count = {good} />
-      <Display text = 'neutral' count = {neutral} />
-      <Display text = 'bad' count = {bad} />
+      <Statisitcs good = {good} bad = {bad} neutral = {neutral} total = {total} average = {average} positive = {positiveFeedbackPercentage} />
     </div>
   )
 }
