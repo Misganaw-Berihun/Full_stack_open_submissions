f165940c0d671de8c1654961955dc100df2b0944
Misganaw-Berihun <misganawbmb@gmail.com>
2023-08-23 22:18:58 +0300

    update Exercise 2.13

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -3,6 +3,8 @@ const Persons = ({persons, setPersons}) => {
 
     const onDelete = (id) => {
         console.log("on delete")
+        const person = persons.find(person => person.id === id)
+        if (confirm(`Delete ${person.name} ?`)){
         newServices 
         .deletePerson(id)
         .then(
@@ -11,7 +13,8 @@ const Persons = ({persons, setPersons}) => {
             // setPersons(persons.filter(person => person.id !== id))
             setPersons(persons.filter(person => person.id !== id))
           }
-        ) 
+        )
+        }
       }
 
     return (
